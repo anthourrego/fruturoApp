0f49edbf00afb07e8aca6ad8c7e659cece3c10cd
Anthony Urrego <anthourrego@gmail.com>
2020-05-30 01:21:13 -0500

    Se termina el modulo de perfiles

---
+++ modulos/usuarios/perfiles/acciones.php
@@ -0,0 +1,179 @@
+<?php
+@session_start();
+header("Access-Control-Allow-Origin:*");
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_raiz=$ruta="";
+while($max_salida>0){
+  if(@is_file($ruta.".htaccess")){
+    $ruta_raiz=$ruta; //Preserva la ruta superior encontrada
+    break;
+  }
+  $ruta.="../";
+  $max_salida--;
+}
+
+require($ruta_raiz . "clases/funciones_generales.php");
+require($ruta_raiz . "clases/Conectar.php");
+require($ruta_raiz . "clases/SSP.php");
+require($ruta_raiz . "clases/Session.php");
+
+$session = new Session();
+
+$usuario = $session->get("usuario");
+
+function lista(){
+  $table      = 'perfiles';
+  // Table's primary key
+  $primaryKey = 'id';
+  // indexes
+  $columns = array(
+              array( 'db' => '`p`.`id`',               'dt' => 'id',             'field' => 'id' ),
+              array( 'db' => '`p`.`nombre`',           'dt' => 'nombre',         'field' => 'nombre' ),
+              array( 'db' => '`p`.`fecha_creacion`',   'dt' => 'fecha_creacion', 'field' => 'fecha_creacion' ),
+              array( 'db' => '`u`.`nombres`',          'dt' => 'creador',        'field' => 'creador',        'as' => 'creador')
+            );
+    
+  $sql_details = array(
+                  'user' => BDUSER,
+                  'pass' => BDPASS,
+                  'db'   => BDNAME,
+                  'host' => BDSERVER
+                );
+      
+  $joinQuery = "FROM `{$table}` AS `p` INNER JOIN `usuarios` AS `u` ON `p`.`fk_creador` = `u`.`id`";
+  $extraWhere= "`p`.`estado` = 1";
+  $groupBy = "";
+  $having = "";
+  return json_encode(SSP::simple($_GET, $sql_details, $table, $primaryKey, $columns, $joinQuery, $extraWhere, $groupBy, $having));
+}
+
+function crear(){
+  $db = new Bd();
+  $db->conectar();
+  $resp = array();
+  global $usuario;
+  $resp['success'] = false;
+
+  if (validarNombre(cadena_db_insertar($_POST["nombre"])) == 0) {
+    $datos = array(
+      ":nombre" => cadena_db_insertar($_POST["nombre"]), 
+      ":fecha_creacion" => date('Y-m-d H:i:s'), 
+      ":estado" => 1, 
+      ":fk_creador" => $usuario["id"]
+    );
+
+    $id_registro = $db->sentencia("INSERT INTO perfiles (nombre, fecha_creacion, estado, fk_creador) VALUES (:nombre, :fecha_creacion, :estado, :fk_creador)", $datos);
+
+    if ($id_registro > 0) {
+      $db->insertLogs("perfiles", $id_registro, "Se crea el perfil {$_POST['nombre']}", $usuario["id"]);
+      $resp['success'] = true;
+      $resp['msj'] = "Se ha creado correctamente {$_POST['nombre']}.";
+    } else {
+      $resp['msj'] = 'Error al realizar el registro.';
+    }
+
+  }else{
+    $resp['msj'] = 'El nombre <b>' . $_REQUEST["nombre"] . '</b> ya se encuentra en uso.';
+  }
+
+  $db->desconectar();
+  return json_encode($resp);
+}
+
+function validarNombre($nombre, $id = 0){
+  $db = new Bd();
+  $db->conectar();
+  $resp = 0;
+
+  if ($id == 0) {
+    $verificar = $db->consulta("SELECT nombre FROM perfiles WHERE nombre = :nombre", array(":nombre" => $nombre));
+  } else {
+    $verificar = $db->consulta("SELECT nombre FROM perfiles WHERE nombre = :nombre AND id != :id", array(":nombre" => $nombre, ':id' => $id));
+  }
+  
+  if ($verificar["cantidad_registros"] > 0) {
+    $resp = $verificar["cantidad_registros"];
+  }
+
+  $db->desconectar();
+
+  return $resp;
+}
+
+function editar(){
+  global $usuario;
+  $db = new Bd();
+  $resp = array();
+  $db->conectar();
+  $datosActuales = datos($_POST["id"]);
+
+  if ($datosActuales != 0) {
+    if (validarNombre(cadena_db_insertar($_POST['nombre']), $_POST["id"]) == 0) {
+      # code...
+      if ($_POST["nombre"] != $datosActuales['nombre']) {
+  
+        $datosSQL = array(
+            ":nombre" => $_POST["nombre"],
+            ":id" => $_POST["id"]
+          );
+  
+        $db->sentencia("UPDATE perfiles SET nombre = :nombre WHERE id = :id", $datosSQL);
+    
+        $db->insertLogs("perfiles", $_POST["id"], "Se edita el perfil {$_POST['nombre']}", $usuario["id"]);
+    
+        $resp["success"] = true;
+        $resp["msj"] = "El perfil {$_POST['nombre']} se ha actualizado correctamente";
+      } else {
+        $resp["success"] = false;
+        $resp["msj"] = "Por favor realize algún cambio";
+      }
+    }else{
+      $resp['msj'] = 'El nombre <b>' . $_REQUEST["nombre"] . '</b> ya se encuentra en uso.';
+    }
+  }else{
+    $resp["success"] = false;
+    $resp["msj"] = "El perfil no es válido";
+  }
+
+
+  $db->desconectar();
+  return json_encode($resp);
+}
+
+function datos($id){
+  $db = new Bd();
+  $db->conectar();
+  $resp = 0;
+
+  $datos = $db->consulta("SELECT * FROM perfiles WHERE id = :id", array(":id" => $id));
+
+  if ($datos["cantidad_registros"] == 1) {
+    $resp = $datos[0];
+  }
+
+  $db->desconectar();
+  return $resp;
+}
+
+function inhabilitar(){
+  global $usuario;
+  $db = new Bd();
+  $db->conectar();
+
+  $db->sentencia("UPDATE perfiles SET estado = 0 WHERE id = :id", array(":id" => $_POST["id"]));
+  $db->insertLogs("perfiles", $_POST["id"], "Se inhabilita el perfil {$_POST['nombre']}", $usuario["id"]);
+
+  $db->desconectar();
+
+  return json_encode(1);
+}
+
+if(@$_REQUEST['accion']){
+  if(function_exists($_REQUEST['accion'])){
+    echo($_REQUEST['accion']());
+  }else{
+    echo 'Accion '.$_REQUEST['accion'].' no Existe';
+  }
+}else{
+  echo 'No se ha seleccionado alguna acción';
+}
---
+++ modulos/usuarios/perfiles/index.php
@@ -0,0 +1,318 @@
+<?php  
+	$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+  $ruta_raiz=$ruta="";
+  while($max_salida>0){
+    if(@is_file($ruta.".htaccess")){
+      $ruta_raiz=$ruta; //Preserva la ruta superior encontrada
+      break;
+    }
+    $ruta.="../";
+    $max_salida--;
+  }
+
+  include_once($ruta_raiz . 'clases/librerias.php');
+  include_once($ruta_raiz . 'clases/sessionActiva.php');
+  include_once($ruta_raiz . 'clases/Permisos.php');
+
+  $session = new Session();
+  $lib = new Libreria;
+  $permisos = new Permisos();
+
+  $usuario = $session->get("usuario");
+
+  if ($permisos->validarPermiso($usuario['id'], 'usuarios_perfiles') == 0) {
+    header('Location: ' . $ruta_raiz . 'modulos/');
+  }
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <?php  
+    echo $lib->jquery();
+    echo $lib->jqueryUI();
+    echo $lib->moment();
+    echo $lib->adminLTE();
+    echo $lib->bootstrap();
+    echo $lib->fontAwesome();
+    echo $lib->sweetAlert2();
+    echo $lib->jqueryValidate();
+    echo $lib->datatables();
+    echo $lib->bootstrapTreeView();
+    echo $lib->proyecto();
+  ?>
+</head>
+<body>
+
+  <!-- Content Header (Page header) -->
+  <div div class="content-header">
+    <div class="container">
+      <div class="row mb-2">
+        <div class="col-12">
+          <h1 class="m-0 text-dark"><i class="fas fa-user-tag"></i> Perfiles</h1>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container">
+      <div class="card">
+        <div class="card-header">
+          <div class="d-flex justify-content-end">
+            <button class="btn btn-success btnCrear" data-toggle="tooltip" title="Crear"><i class="fas fa-plus"></i></button>
+          </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="tabla" class="table table-bordered table-hover table-sm w-100">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Creador</th>
+                <th scope="col">Fecha creación</th>
+                <th scope="col">Acciones</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+
+  <!-- Modal Producto -->
+  <div class="modal fade" id="modalCrear" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="tituloModal"><i class="fas fa-plus"></i> Crear perfil</h5>
+        </div>
+        <form id="formCrear" autocomplete="off">
+          <input type="hidden" name="accion" value="crear">
+          <input type="hidden" name="id" value="0">
+          <div class="modal-body">
+            <div class="form-group">
+              <label for="nombre">Nombre <span class="text-danger">*</span></label>
+              <input type="text" name="nombre" class="form-control" placeholder="Escriba el nombre del perfil" required autocomplete="off">
+            </div>
+          </div>
+          <div class="modal-footer d-flex justify-content-between">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fas fa-times"></i> Cerrar</button>
+            <button id="btnCrear" type="submit" class="btn btn-primary"><i class="fas fa-paper-plane"></i> Enviar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+<?php 
+  echo $lib->cambioPantalla();
+?>
+<script>
+  $(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //Se abre la modal para crear
+    $('.btnCrear').on("click", function(){
+      $("#tituloModal").html(`<i class="fas fa-plus"></i> Crear perfil`);
+      $("#formCrear :input[name='accion']").val('crear');
+      $("#modalCrear").modal("show");
+      $("#formCrear :input[name='nombre']").focus();
+    });
+
+    //Editar Usuario
+    $(document).on("click", ".btnEditar", function(){
+      let datos = $(this).data("datos");
+      $("#tituloModal").html(`<i class="fas fa-edit"></i> Editar perfil | ` + datos['nombre']);
+      $("#formCrear :input").removeClass("is-valid");
+      $("#formCrear :input").removeClass("is-invalid");
+
+      $("#formCrear :input[name='accion']").val('editar');
+      $("#formCrear :input[name='id']").val(datos['id']);
+      $("#formCrear :input[name='nombre']").val(datos['nombre']);
+
+      $("#modalCrear").modal("show");
+      $("#formCrear :input[name='nombre']").focus();
+    });
+
+    //Acciones al cerrar la modal
+    $('#modalCrear').on('hidden.bs.modal', function (e) {
+      if($("#formCrear :input[name='accion']").val() == 'editar'){
+        $("#formCrear")[0].reset();
+      }
+    });
+
+    $("#formCrear").submit(function(event){
+      event.preventDefault();
+      if($("#formCrear").valid()){
+        $.ajax({
+          type: "POST",
+          url: "acciones",
+          cache: false,
+          contentType: false,
+          dataType: 'json',
+          processData: false,
+          data: new FormData(this),
+          beforeSend: function(){
+            $('#formCrear :input').attr("disabled", true);
+            //Desabilitamos el botón
+            $('#btnCrear').html(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Enviando...`);
+            $("#btnCrear").attr("disabled" , true);
+          },
+          success: function(data){
+            if (data.success) {
+              $("#tabla").DataTable().ajax.reload();
+              $("#formCrear")[0].reset();
+              $("#formCrear :input").removeClass("is-valid");
+              $("#formCrear :input").removeClass("is-invalid");
+              $("#modalCrear").modal("hide");
+              Swal.fire({
+                toast: true,
+                position: 'bottom-end',
+                icon: 'success',
+                title: data.msj,
+                showConfirmButton: false,
+                timer: 5000
+              });
+            }else{
+              Swal.fire({
+                icon: 'error',
+                html: data.msj
+              })
+            }
+          },
+          error: function(){
+            //Habilitamos el botón
+            Swal.fire({
+              icon: 'error',
+              html: 'Error al registrar.'
+            });
+            //Habilitamos el botón
+            $('#formCrear :input').attr("disabled", false);
+            $('#btnCrear').html(`<i class="fas fa-paper-plane"></i> Enviar`);
+            $("#btnCrear").attr("disabled", false);
+          },
+          complete: function(){
+            //Habilitamos el botón
+            $('#formCrear :input').attr("disabled", false);
+            $('#btnCrear').html(`<i class="fas fa-paper-plane"></i> Enviar`);
+            $("#btnCrear").attr("disabled", false);
+          }
+        });
+      }
+    });
+
+    listaTabla();
+  });
+
+  function inhabilitar(datos){
+    Swal.fire({
+      title: "¿Estas seguro de inhabilitar el perfil " + datos['nombre'] + "?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '<i class="far fa-trash-alt"></i> Si',
+      cancelButtonText: '<i class="fa fa-times"></i> No'
+    }).then((result) => {
+      if (result.value) {
+        $.ajax({
+          url: 'acciones',
+          type: 'POST',
+          dataType: 'json',
+          data: {
+            accion: "inhabilitar", 
+            id: datos['id'],
+            nombre: datos['nombre']
+          },
+          success: function(data){
+            if (data == 1) {
+              $("#tabla").DataTable().ajax.reload();
+              Swal.fire({
+                toast: true,
+                position: 'bottom-end',
+                icon: 'success',
+                title: "Se ha inhabilitado el perfil " + datos['nombre'],
+                showConfirmButton: false,
+                timer: 5000
+              });
+            }else{
+              Swal.fire({
+                icon: 'warning',
+                html: "Error al inhabilitar el perfil " + datos['nombre']
+              })
+            }
+          },
+          error: function(){
+            Swal.fire({
+              icon: 'error',
+              html: 'No se han enviado los datos'
+            })
+          }
+        });
+      }
+    });
+  }
+
+  function listaTabla(){
+    $("#tabla").DataTable({
+      stateSave: true,
+      responsive: true,
+      processing: true,
+      serverSide: true,
+      pageLength: 25,
+      language: {
+        url: "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+      },
+      ajax: {
+          url: "acciones",
+          type: "GET",
+          dataType: "json",
+          data: {
+            accion: 'lista'
+          },
+          complete: function(){
+            $('[data-toggle="tooltip"]').tooltip('hide');
+            $('[data-toggle="tooltip"]').tooltip();
+            cerrarCargando();
+          }
+      },
+      columns: [
+        { data: "id" },
+        { data: "nombre" },
+        { data: "creador" },
+        { data: "fecha_creacion"},
+        {
+          "render": function (nTd, sData, oData, iRow, iCol) {
+            return `<div class="d-flex justify-content-center">
+                      <button type="button" class="btn btn-primary btn-sm mx-1 btnEditar" data-toggle="tooltip" title="Editar" data-datos='${JSON.stringify(oData)}'><i class="far fa-edit"></i></button>
+                      <button type="button" class="btn btn-danger btn-sm mx-1" onClick='inhabilitar(${JSON.stringify(oData)})' data-toggle="tooltip" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+                    </div>`;
+          }
+        }
+      ],
+      columnDefs: [
+        {
+          className: "dt-center",
+          targets: "_all"
+        },
+        {
+          targets: [0],
+          visible: false
+        }
+      ],
+      lengthChange: true,
+      order: [
+        [0, "asc"]
+      ], //Ordenar (columna,orden)
+    });
+  }
+</script>
+</html>
